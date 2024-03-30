e811ee2897535bbc96750f8c488b6b7de653f61e
captain-sal <mssalonigupta@gmail.com>
2024-03-30 17:12:03 +0530

    first commit

---
+++ src/page/index.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row-efa'>
+        <button className='rectangle'>
+          <span className='login-sign-up'>Login/Sign Up</span>
+        </button>
+        <span className='bite-by-byte'>BiteByByte</span>
+        <span className='blogs'>Blogs</span>
+        <span className='health'>Health</span>
+        <span className='community'>Community</span>
+        <div className='rectangle-1'>
+          <span className='journey-to-wellness'>
+            Are you ready to take control of your journey to wellness?
+            BiteByByte is here to support you every step of the way.
+          </span>
+          <button className='rectangle-2'>
+            <div className='ellipse' />
+            <span className='explore-platform'>Explore the platform</span>
+            <div className='arrow' />
+          </button>
+          <span className='unique'>UNIQUE</span>
+          <span className='your-mind'>Your mind is</span>
+          <span className='your-mental'>Your mental</span>
+          <span className='health-3'>HEALTH</span>
+          <span className='should-be-too'>should be too</span>
+        </div>
+        <div className='image' />
+      </div>
+      <div className='flex-row-e'>
+        <div className='rectangle-4' />
+        <div className='rectangle-5' />
+        <div className='rectangle-6' />
+        <div className='rectangle-7'>
+          <div className='rectangle-8'>
+            <div className='line' />
+            <span className='lorem-ipsum'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation
+            </span>
+          </div>
+        </div>
+      </div>
+      <button className='rectangle-9'>
+        <div className='flex-column-be'>
+          <span className='start-your'>Start your</span>
+          <span className='wellness-journey'>Wellness Journey</span>
+        </div>
+        <div className='arrow-a' />
+      </button>
+      <div className='rectangle-b' />
+      <div className='flex-row-eb'>
+        <div className='vector'>
+          <button className='rectangle-button'>
+            <span className='get-started-span'>Get started</span>
+            <div className='arrow-div' />
+          </button>
+          <div className='flex-row-eca-div'>
+            <div className='image-div' />
+            <span className='nutrition-plans-span'>
+              Tailor-made nutrition plans to meet your unique needs and
+              preferences. Achieve balance and nourishment effortlessly.
+            </span>
+          </div>
+        </div>
+        <div className='vector-div'>
+          <div className='rectangle-div'>
+            <span className='view-blogs-span'>View blogs & articles</span>
+            <div className='arrow-div-c' />
+          </div>
+          <div className='flex-row-div'>
+            <span className='educational-resources-span'>
+              Our curated library of educational resources covers everything
+              from understanding anorexia to practical tips for recovery,
+              empowering users with the knowledge and insight they need to make
+              informed decisions about their health.
+            </span>
+            <div className='image-d' />
+          </div>
+        </div>
+        <div className='vector-e'>
+          <span className='community-connection'>Community Connection</span>
+          <div className='flex-row-e-f'>
+            <div className='ellipse-10' />
+            <span className='share-your-story'>Share your story</span>
+            <div className='arrow-11' />
+          </div>
+        </div>
+      </div>
+      <div className='vector-12' />
+    </div>
+  );
+}
